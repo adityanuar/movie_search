1f013165111817d32680fea55ab12aa091524b8d
Adit <adityanuar@gmail.com>
2022-08-02 00:45:54 +0700

    zoom effect

--- front_end/components/contents/card.jsx
+++ front_end/components/contents/card.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Card = ({ title, poster, year, type }) => {
   console.log(title, poster);
   const colorRGB = {
@@ -6,16 +8,33 @@ const Card = ({ title, poster, year, type }) => {
     green: [50, 230, 95],
     red: [247, 80, 77],
   };
+  const [isHover, setIsHover] = useState(false);
   return (
-    <div
-      className="w-52 h-52 bg-blue-200 rounded-2xl shadow-lg"
-      style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(${colorRGB.red.join(
-          ","
-        )})), url(${poster}`,
-      }}
-    >
-      <div className="flex flex-col m-5">
+    <div className="w-52 h-52 bg-blue-200 rounded-2xl shadow-lg overflow-hidden relative">
+      <div
+        className="w-full h-full absolute transition-all"
+        style={
+          isHover
+            ? {
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(${colorRGB.red.join(
+                  ","
+                )})), url(${poster})`,
+                transform: "scale(1.2)",
+              }
+            : {
+                backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(${colorRGB.red.join(
+                  ","
+                )})), url(${poster})`,
+                transform: "scale(1)",
+              }
+        }
+        onMouseEnter={() => {
+          console.log("hovering");
+          setIsHover(true);
+        }}
+        onMouseLeave={() => setIsHover(false)}
+      ></div>
+      <div className="flex flex-col m-5 absolute">
         <div className="text-white font-semibold">{title}</div>
         <div className="text-white">{year}</div>
         <div className="text-white font-light grow justify-self-end">
